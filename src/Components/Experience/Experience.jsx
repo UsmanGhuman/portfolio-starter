d1ea22c630ddf4dbe7869595434f638f9ba51dba
UsmanGhuman <usmanghuman072@gmail.com>
2025-08-03 15:14:50 +0500

    your update message

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -4,17 +4,17 @@ export default function Experience() {
   return (
    <div className="experience">
     <div className="achievement">
-        <div className="circle">8+</div>
+        <div className="circle">Fresh</div>
         <span>years</span>
         <span>Experience</span>
     </div>
     <div className="achievement">
-        <div className="circle">20+</div>
+        <div className="circle">2+</div>
         <span>completed</span>
         <span>Projects</span>
     </div>
     <div className="achievement">
-        <div className="circle">5+</div>
+        <div className="circle">2 months+</div>
         <span>companies</span>
         <span>work</span>
     </div>
